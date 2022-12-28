4a9d53f5d9f384e30dd839112c71733d0d81189b
kuprelweronika <110784022+kuprelweronika@users.noreply.github.com>
2022-12-28 21:48:21 +0100

    adding calculator fun

--- quiz.js
+++ quiz.js
@@ -1,20 +1,25 @@
-if (
-  confirm(
-    "Czy chcesz zostać junior frontend deweloperem?" +
-      String.fromCodePoint(0x1f41e)
-  )
-) {
-  alert("Ucz się regularnie");
-  let godziny = prompt(
-    "Ile czasu regularnie poświęcasz na naukę?" + String.fromCodePoint(0x1211e)
-  );
-  if (godziny > 7) {
-    alert("Wow, lecisz jak burza!" + String.fromCodePoint(0x1b41e));
-  } else if (godziny == 0) {
-    alert("Oby to był tylko stan przejściowy!" + String.fromCodePoint(0x1f46b));
-  } else {
-    alert("Powoli do przodu");
+let pierwsza = prompt("Podaj pierwszą liczbę");
+
+let znak = prompt("Podaj operator + - ( / lub %");
+
+let druga = prompt("Podaj drugą liczbę");
+
+if (znak === "+") {
+  let wynik = alert(parseInt(pierwsza) + parseInt(druga));
+}
+if (znak === "-") {
+  let wynik = alert(parseInt(pierwsza) - parseInt(druga));
+}
+
+if (znak === "*") {
+  let wynik = alert(parseInt(pierwsza) * parseInt(druga));
+}
+if (znak === "/") {
+  if (druga === "0") {
+    alert("Nie dziel przez zero cholero!");
+    alert(parseInt(pierwsza) / parseInt(druga));
   }
-} else {
-  alert("To co tutaj robisz?");
+}
+if (znak === "%") {
+  let wynik = alert(parseInt(pierwsza) % parseInt(druga));
 }
