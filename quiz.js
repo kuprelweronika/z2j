368fc5d2a353496b6f1025ec07dd198fa696bbcc
kuprelweronika <110784022+kuprelweronika@users.noreply.github.com>
2023-01-04 11:26:02 +0100

    improve multipy and divide

--- quiz.js
+++ quiz.js
@@ -3,22 +3,22 @@ function add(num1, num2) {
 }
 
 function subtract(num1, num2) {
-  return (num1 = num1 - num2);
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-  return (num1 = num1 * num2);
+  return num1 * num2;
 }
 
 function divide(num1, num2) {
   if (num2 === 0) {
     alert("Nie dziel przez zero cholero");
   } else {
-    return (num1 = num1 / num2);
+    return num1 / num2;
   }
 }
 function mod(num1, num2) {
-  return (num1 = num1 % num2);
+  return num1 % num2;
 }
 
 let num1 = parseFloat(prompt("Podaj pierwszą liczbę"));
@@ -37,21 +37,21 @@ do {
       break;
     case "*":
       num2 = parseFloat(prompt("Podaj drugą liczbę"));
-      alert(multiply(num1, num2));
+      alert((num1 = multiply(num1, num2)));
       sym = prompt("Podaj operator + - ( / lub %");
 
       break;
     case "/":
       num2 = parseFloat(prompt("Podaj drugą liczbę"));
 
-      alert(divide(num1, num2));
+      alert((num1 = divide(num1, num2)));
       sym = prompt("Podaj operator + - ( / lub %");
 
       break;
     case "%":
       num2 = parseFloat(prompt("Podaj drugą liczbę"));
 
-      alert(mod(num1, num2));
+      alert((num1 = mod(num1, num2)));
       sym = prompt("Podaj operator + - ( / lub %");
       break;
   }
