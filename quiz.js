81e64c4559baf3c3c721be5484df884ab4de8830
kuprelweronika <110784022+kuprelweronika@users.noreply.github.com>
2023-02-05 21:42:08 +0100

    add win validation with 1 condition

--- quiz.js
+++ quiz.js
@@ -30,24 +30,27 @@ for (let i = 1; i < 101; i++) {
 
   document.body.appendChild(btn);
 }
+let steps = {
+  X: [],
+  O: [],
+};
 
 function clicked() {
   id = window.event.target.id;
-  console.log(id);
   btnUser = window.document.getElementById(id);
   if (btnUser.value.length > 0) {
     alert("Pole zajęte!!! Wybierz inne!");
   } else {
     btnUser.value = "X";
     btnUser.innerHTML = "X";
+    steps.X.push(parseInt(id));
     computerPlay();
-    console.log(id);
   }
 }
 
 function computerPlay() {
   let click = 1;
-  while (click < 9) {
+  while (click < 100) {
     btnComp = window.document.getElementById(click);
 
     if (btnComp?.value.length > 0) {
@@ -55,6 +58,8 @@ function computerPlay() {
     } else {
       btnComp.value = "O";
       btnComp.innerHTML = "O";
+      steps.O.push(parseInt(id));
+
       click++;
       break;
     }
@@ -76,15 +81,38 @@ function resetByButton() {
   document.getElementById("reset").remove();
 }
 
+winConditions = [
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+  [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
+  [31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+  [41, 42, 43, 44, 45, 46, 47, 48, 49, 50],
+  [51, 52, 53, 54, 55, 56, 57, 58, 59, 60],
+  [61, 62, 63, 64, 65, 66, 67, 68, 69, 70],
+  [71, 72, 73, 74, 75, 76, 77, 78, 79, 80],
+  [81, 82, 83, 84, 85, 86, 87, 88, 89, 90],
+  [91, 92, 93, 94, 95, 96, 97, 98, 99, 100],
+];
+
 function checkWin() {
-  if ((document.getElementById(1).value === "X" && document.getElementById(2).value === "X" && document.getElementById(3).value === "X") || (document.getElementById(1).value === "X" && document.getElementById(4).value === "X" && document.getElementById(7).value === "X") || (document.getElementById(1).value === "X" && document.getElementById(5).value === "X" && document.getElementById(9).value === "X") || (document.getElementById(3).value === "X" && document.getElementById(5).value === "X" && document.getElementById(7).value === "X") || (document.getElementById(2).value === "X" && document.getElementById(5).value === "X" && document.getElementById(8).value === "X") || (document.getElementById(4).value === "X" && document.getElementById(5).value === "X" && document.getElementById(6).value === "X") || (document.getElementById(3).value === "X" && document.getElementById(6).value === "X" && document.getElementById(9).value === "X") || (document.getElementById(7).value === "X" && document.getElementById(8).value === "X" && document.getElementById(9).value === "X")) {
+  if (
+    (win = winConditions.some((winCondition) => {
+      return winCondition.every((condition) => steps.X.includes(condition));
+    }))
+  ) {
     alert("you win");
     let resetBtn = document.createElement("button");
     resetBtn.innerHTML = "RESET";
     resetBtn.id = "reset";
     resetBtn.onclick = resetByButton;
     document.body.appendChild(resetBtn);
-  } else if ((document.getElementById(1).value === "O" && document.getElementById(2).value === "O" && document.getElementById(3).value === "O") || (document.getElementById(1).value === "O" && document.getElementById(4).value === "O" && document.getElementById(7).value === "O") || (document.getElementById(1).value === "O" && document.getElementById(5).value === "O" && document.getElementById(9).value === "O") || (document.getElementById(3).value === "O" && document.getElementById(5).value === "O" && document.getElementById(7).value === "O") || (document.getElementById(2).value === "O" && document.getElementById(5).value === "O" && document.getElementById(8).value === "O") || (document.getElementById(4).value === "O" && document.getElementById(5).value === "O" && document.getElementById(6).value === "O") || (document.getElementById(3).value === "O" && document.getElementById(6).value === "O" && document.getElementById(9).value === "O") || (document.getElementById(7).value === "X" && document.getElementById(8).value === "X" && document.getElementById(9).value === "X")) {
+  }
+
+  if (
+    (win = winConditions.some((winCondition) => {
+      return winCondition.every((condition) => steps.O.includes(condition));
+    }))
+  ) {
     alert("comp win");
     let resetBtn = document.createElement("button");
     resetBtn.innerHTML = "RESET";
