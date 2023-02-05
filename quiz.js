1a1c8f27ce5a96c101faae50dccb741937755c23
kuprelweronika <110784022+kuprelweronika@users.noreply.github.com>
2023-02-05 20:56:58 +0100

    corrections in reset function

--- quiz.js
+++ quiz.js
@@ -63,7 +63,7 @@ function computerPlay() {
 }
 
 function reset() {
-  for (let i = 1; i < 100; i++) {
+  for (let i = 1; i < 101; i++) {
     let reset = window.document.getElementById(i);
     reset.value = "";
     reset.innerHTML = "";
